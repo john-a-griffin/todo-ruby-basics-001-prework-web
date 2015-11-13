4910da9e986b2ddb61e302cbbc4f1d7372685b5f
john-a-griffin <john.griffin.simontech@gmail.com>
2015-11-13 17:20:31 +0000

    finished

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,33 @@
 def division(num1, num2)
+  return num1/num2
+end
+
+def assign_variable(name = "john")
+  puts name
+  return name
+end
+
+def argue(phrase = "I'm right and you are wrong!")
+  puts phrase
+  return phrase
+end
+
+def greeting(name, language)
  
-end
-
-def assign_variable(value)
-  
-end
-
-def argue
-  
-end
-
-def greeting
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 
 end
 
-def return_a_value
+def return_a_value(phrase = "Nice")
+  return phrase
 
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase = "expert")
+  return phrase
 end
 
-def pizza_party
-
+def pizza_party(phrase = "cheese")
+  puts "cheese"
+  return phrase
 end
